8753badd35ed55dce9b42a6c791b07c078fcaf85
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-06-15 10:42:03 +0900

    Generic Pool Manager의 이상한점 수정

--- Assets/Scripts/Base/GenericPoolManager.cs
+++ Assets/Scripts/Base/GenericPoolManager.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GenericPoolManager<T> where T : Object
+public static class GenericPoolManager
 {
     private static Dictionary<string, object> poolDict = new Dictionary<string, object>();
 
-    public static void CratePool(string key, T obj, Transform parent , int count)
+    public static void CratePool<T>(string key, T obj, Transform parent, int count) where T : Object
     {
         poolDict.Add(key, new GenericPool<T>(obj, parent, count));
     }
 
-    public static GenericPool<T> GetPool(string key)
+    public static GenericPool<T> GetPool<T>(string key) where T : Object
     {
         return poolDict[key] as GenericPool<T>;
     }
